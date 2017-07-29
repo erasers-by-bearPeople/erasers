f93a41b561205544e6b8fc63187c23d812c930a2
Shayne Mihalka <shayne@mihalka.com>
2017-07-28 19:41:42 -0700

    put up working store for product

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,20 +1,9 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 import { singleProduct } from '../store'
-// const props = {
-//   product: {
-//     id: 1,
-//     title: 'Crayon, Crayoff',
-//     description: 'No worries about drawings on the wall with this colorful set, watch as your confused child tries to color to no avail',
-//     price: 350,
-//     category: 'Novelty',
-//     inventory: 100,
-//     image: '/images/crayons.jpg'
-//   }
-// }
 
-class SingleProduct extends React.Component() {
+class SingleProduct extends Component {
 
   componentDidMount() {
     this.props.fetchSingleProduct(this.props.match.params.productId)
@@ -34,34 +23,35 @@ class SingleProduct extends React.Component() {
           {product.description}
         </h4>
         <table className='text-center'>
-          <tr>
-            <td>
-              <h3>${product.price / 100}</h3>
-            </td>
-            <td>
-              <button className='btn btn-info' value={product.id}>
-                Add To Cart
-          </button>
-            </td>
-            <td className='quantity'>
-              <label>Qty</label>
-              <select id={product.id}>
-                {
-                  [1, 2, 3, 4, 5, 6, 7, 8].map(num => {
-                    return <option key={num}>{num}</option>
-                  })
-                }
-              </select>
-            </td>
-          </tr>
-          <tr className='prod-reviews'>REVIEWS HERE</tr>
+          <tbody>
+            <tr>
+              <td>
+                <h3>${product.price / 100}</h3>
+              </td>
+              <td>
+                <button className='btn btn-info' value={product.id}>
+                  Add To Cart
+                </button>
+              </td>
+              <td className='quantity'>
+                <label>Qty</label>
+                <select id={product.id}>
+                  {
+                    [1, 2, 3, 4, 5, 6, 7, 8].map(num => {
+                      return <option key={num}>{num}</option>
+                    })
+                  }
+                </select>
+              </td>
+            </tr>
+            {/* <tr className='prod-reviews'>REVIEWS HERE</tr> */}
+          </tbody>
         </table>
 
       </div>
 
     )
   }
-
 }
 
 const mapStateToProps = (state) => {
