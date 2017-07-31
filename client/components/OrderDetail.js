08a3b6e8e6585edf4995e804f0934a52c7263359
Shayne Mihalka <shayne@mihalka.com>
2017-07-31 14:29:41 -0700

    merge with latest

--- client/components/OrderDetail.js
+++ client/components/OrderDetail.js
@@ -38,7 +38,7 @@ class LineItem extends Component {
                   //adding up total (have another function for this)
                   total += item.price * item.quantity
                   return (<tr key={item.id}>
-                    <td><img src={item.product.image} height='50px'/></td>
+                    <td><Link to={`/products/${item.product.id}`}><img src={item.product.image} height='50px'/></Link></td>
                     <td>{item.product.title}</td>
                     <td>{item.product.category}</td>
                     <td>{formatter.format(item.price/100)}</td>
@@ -108,7 +108,6 @@ const mapDispatchToProps = (dispatch) => {
     handleOnChange(event){
       const value = +event.target.value
       const id = +event.target.dataset.id
-      console.log({quantity: value, id: id})
       dispatch(updateQuanityLineItem({quantity: value, id: id}))
     }
   }
