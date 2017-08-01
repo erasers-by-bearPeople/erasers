fbf9f4aaef98ab4d6e5d82483102e96caa00ace3
Lina Jones <lina.jones630@gmail.com>
2017-08-01 10:08:55 -0600

    fix - move admin reducer and add to store

---
+++ client/store/manageOrders.js
@@ -0,0 +1,40 @@
+/* -----------------    IMPORTS     ------------------ */
+
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+
+
+const ADMIN_GET_ORDERS = 'ADMIN_GET_ORDERS'
+
+/**
+* ACTION CREATORS
+*/
+
+const adminGetUserOrders = adminOrders => ({type: ADMIN_GET_ORDERS, adminOrders})
+
+/**
+ * THUNK CREATORS
+ */
+
+export const adminGetAllUserOrders = ()=>
+  dispatch =>
+    axios.get('/api/orders/')
+      .then(res =>
+        dispatch(adminGetUserOrders(res.data)))
+      .catch(err => console.log(err))
+
+/**
+* REDUCER
+*/
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case ADMIN_GET_ORDERS:
+      return action.adminOrders
+    default:
+      return state
+  }
+}
